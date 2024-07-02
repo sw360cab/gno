762fc9938d4fc252503e808e12e601e56432b08f
github-action-benchmark <github@users.noreply.github.com>
2024-07-02 12:52:48 +0000

    add Go Benchmark (go) benchmark result for c296e1dccf7320cf7de13fffb9a94821df5c4b32

---
+++ dev/bench/data.js
@@ -0,0 +1,80 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1719924768386,
+  "repoUrl": "https://github.com/sw360cab/gno",
+  "entries": {
+    "Go Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "sw360cab@gmail.com",
+            "name": "Sergio Maria Matone",
+            "username": "sw360cab"
+          },
+          "committer": {
+            "email": "sw360cab@gmail.com",
+            "name": "Sergio Maria Matone",
+            "username": "sw360cab"
+          },
+          "distinct": true,
+          "id": "c296e1dccf7320cf7de13fffb9a94821df5c4b32",
+          "message": "using local gh-pages",
+          "timestamp": "2024-07-02T14:52:11+02:00",
+          "tree_id": "3e7828b87fe28643312c83e00daab97732d39339",
+          "url": "https://github.com/sw360cab/gno/commit/c296e1dccf7320cf7de13fffb9a94821df5c4b32"
+        },
+        "date": 1719924766796,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkGoLevelDBRandomReadsWrites",
+            "value": 6401,
+            "unit": "ns/op\t     560 B/op\t      11 allocs/op",
+            "extra": "210110 times"
+          },
+          {
+            "name": "BenchmarkGoLevelDBRandomReadsWrites - ns/op",
+            "value": 6401,
+            "unit": "ns/op",
+            "extra": "210110 times"
+          },
+          {
+            "name": "BenchmarkGoLevelDBRandomReadsWrites - B/op",
+            "value": 560,
+            "unit": "B/op",
+            "extra": "210110 times"
+          },
+          {
+            "name": "BenchmarkGoLevelDBRandomReadsWrites - allocs/op",
+            "value": 11,
+            "unit": "allocs/op",
+            "extra": "210110 times"
+          },
+          {
+            "name": "BenchmarkGoLevelDBRandomReadsWrites",
+            "value": 6514,
+            "unit": "ns/op\t     586 B/op\t      11 allocs/op",
+            "extra": "221542 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkGoLevelDBRandomReadsWrites - ns/op",
+            "value": 6514,
+            "unit": "ns/op",
+            "extra": "221542 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkGoLevelDBRandomReadsWrites - B/op",
+            "value": 586,
+            "unit": "B/op",
+            "extra": "221542 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkGoLevelDBRandomReadsWrites - allocs/op",
+            "value": 11,
+            "unit": "allocs/op",
+            "extra": "221542 times\n2 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
